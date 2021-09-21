c9a9a56d611e01989f8895e3ed124741c5367957
oizy404 <71779024+oizy404@users.noreply.github.com>
2021-09-22 02:48:57 +0800

    dentalmvc

---
+++ app/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+    //Load config
+    require_once 'config/config.php';
+
+    //load libraries
+    // require_once 'libraries/core.php';
+    // require_once 'libraries/controller.php';
+    // require_once 'libraries/database.php';
+
+    //Autoload Core Libraries
+    spl_autoload_register(function($className){
+        require_once 'libraries/' . $className . '.php';
+    });
---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    //DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'dental_assoc');
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT', 'http://localhost/dentalmvc');
+    //Site Name
+    define('SITENAME', 'DentalMVC');
